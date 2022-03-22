34d8b6da4f9d5c8fb66e91e81fc22e5f56a33845
Inty Mendoza <intymendoza@gmail.com>
2022-03-22 14:23:22 -0300

    back to original

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
-const bodyParser = require("body-parser");
-const mercadopago = require("mercadopago");
 
 var app = express();
-app.use(bodyParser.urlencoded({extended: true}));
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -22,6 +19,4 @@ app.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
 
-app.listen(port, function(){
-    console.log("Server up and runnig at port 3000");
-});
+app.listen(port);
